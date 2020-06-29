7ef7b294dbd863c30787af09f773e337544a1f91
austinmcdaniel <ausome99@gmail.com>
2020-06-29 15:11:41 -0600

    formatted blog pages

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -1,38 +1,56 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
 export default class BlogDetail extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            currentId: this.props.match.params.slug,
-            blogItem: {}
-        };
-    }
+    this.state = {
+      currentId: this.props.match.params.slug,
+      blogItem: {}
+    };
+  }
 
-    getBlogItems() {
-        axios.get(
-            `https://austinmcdaniel.devcamp.space/portfolio/portfolio_blogs/${this.state
-            .currentId}`
-        )
-        .then(response => {
-            console.log("getBlogItems response", response);
-        })
-        .catch(error => {
-            console.log("getBlogItem error", error);
+  getBlogItem() {
+    axios
+      .get(
+        `https://austinmcdaniel.devcamp.space/portfolio/portfolio_blogs/${this.state
+          .currentId}`
+      )
+      .then(response => {
+        this.setState({
+          blogItem: response.data.portfolio_blog
         });
-    }
+      })
+      .catch(error => {
+        console.log("getBlogItem error", error);
+      });
+  }
 
-    componentDidMount() {
-        this.getBlogItem();
-    }
+  componentDidMount() {
+    this.getBlogItem();
+  }
 
-    render() {
-        return (
-            <div>
-                <h1>Blog detail</h1>
-            </div>
-        );
-    }
+  render() {
+    const {
+      title,
+      content,
+      featured_image_url,
+      blog_status
+    } = this.state.blogItem;
+
+    return (
+      <div className="blog-container">
+        <div className="content-container">
+          <h1>{title}</h1>
+
+          <div className="featured-image-wrapper">
+            <img src={featured_image_url} />
+          </div>
+
+          <div className="content">{content}</div>
+        </div>
+      </div>
+    );
+  }
 }
